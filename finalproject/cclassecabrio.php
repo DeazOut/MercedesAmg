52dfc065343fb4a40f0614213ce313a218d3848d
DeazOut <88485210+DeazOut@users.noreply.github.com>
2024-02-15 18:35:57 +0300

    first

---
+++ finalproject/cclassecabrio.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>C CLASSE CABRIO</title>
+</head>
+<body>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Koulen&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="cssccabrio.css">
+    <link rel="icon" href="logoamg.png" type="image/png">
+
+</head>
+<body>
+    <header>
+        <div class="menu">
+            <a style=" position: fixed; z-index: 999999;" href="index.html"> <img id="logo"  src="https://www.mercedes-amg.com/etc/clientlibs/amg/amg.base/assets/images/svg/amg-logo.svg" alt=""></a>
+            <!DOCTYPE  svg   PUBLIC '-//W3C//DTD SVG 1.1//EN'  'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'><svg id="lupa" enable-background="new 0 0 32 32" height="32px" version="1.1" viewBox="0 0 32 32" style=" position: fixed; z-index: 999999;" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="search_magnifier_magnifying_glass_loupe"><g id="search_funds_x2C__magnifying_glass_x2C__magnifier_x2C__loupe_1_"><g id="analysis_2_"><g><g><g><path d="M23.586,23.586c0.122-0.122,0.262-0.217,0.408-0.299l-2.276-2.277c-0.195-0.195-0.512-0.195-0.707,0        c-0.195,0.196-0.195,0.512,0,0.708l2.271,2.271C23.368,23.846,23.464,23.707,23.586,23.586z" fill="#263238"/><path d="M28.5,31c-0.667,0-1.295-0.26-1.768-0.732l-3.5-3.5C22.76,26.295,22.5,25.668,22.5,25        s0.26-1.295,0.732-1.768c0.906-0.906,2.629-0.906,3.535,0l3.5,3.5C30.74,27.205,31,27.832,31,28.5s-0.26,1.295-0.732,1.768        S29.167,31,28.5,31z M25,23.52c-0.407,0-0.793,0.152-1.061,0.42C23.656,24.223,23.5,24.6,23.5,25s0.156,0.777,0.439,1.061        l3.5,3.5c0.567,0.566,1.554,0.566,2.121,0C29.844,29.277,30,28.9,30,28.5s-0.156-0.777-0.439-1.061l-3.5-3.5        C25.793,23.672,25.407,23.52,25,23.52z" fill="#263238"/></g><g><path d="M13,22.45c-0.276,0-0.5-0.224-0.5-0.5s0.224-0.5,0.5-0.5c3.767,0,7.035-2.404,8.133-5.981        c0.081-0.264,0.361-0.415,0.625-0.331c0.264,0.081,0.413,0.36,0.332,0.624C20.861,19.763,17.209,22.45,13,22.45z" fill="#263238"/></g><path d="M13,25C6.383,25,1,19.617,1,13S6.383,1,13,1s12,5.383,12,12S19.617,25,13,25z M13,2       C6.935,2,2,6.935,2,13s4.935,11,11,11s11-4.935,11-11S19.065,2,13,2z" fill="#263238"/></g></g></g></g><circle cx="22" cy="13" fill="#263238" r="0.5"/></g></svg>
+            <input style=" position: fixed; z-index: 999999;" type="checkbox" id="nav-toggle" hidden>
+            <form style=" position: fixed; z-index: 999999;" id="find">
+                <p><input type="search" name="q" placeholder="Поиск по сайту"> 
+            </form>
+
+            <input type="checkbox" id="nav-toggle" hidden>
+    
+            <nav class="nav">
+     
+    
+            <label for="nav-toggle" class="nav-toggle" onclick></label>
+            <h2 class="logo"> 
+                <a href="index.html">Mercedes AMG</a> 
+            </h2>
+            <ul>
+                <li><a href="cars.html">Машины</a>
+                <li><a href="history.html">История</a>
+                <li><a href="map.html">Дилеры</a>
+                <li><a href="index.html">Основная</a>
+            </ul>
+        </nav>
+		
+        <div class="mask-content"></div>
+        
+        </div>
+        <div class="vid">
+            <div style="width: 1920px; overflow: hidden;" id="car">
+                <svg style="overflow: hidden;" id="car1">
+                    <text style="overflow: hidden;" x="50%" y="50%" dy=".35em" text-anchor="middle">
+                        AMG C63 CABRIO
+                    </text>
+                </svg>
+            </div>
+            <img style="width: 1920px;" class="vid1" src="https://foto.carexpert.ru/img/foto1680/mercedes/mersc529.jpg">
+        </div>
+        <div class="buywidget">
+            <div class="buy">
+                <h1 style="color: white;" class="rubik"> Mercedes-AMG C63 </h1>
+                <a class="rubik" style="color: white; font-size: 40px;">━━━━━━━━</a>
+                <div style="display: flex; align-items: center; justify-content: center;">
+                    <a class="rubik butbuy" id="openModal" style="font-weight: 800; font-size: 25px;">Приобрести Автомобиль</a>
+                </div>
+                
+            </div>  
+        </div>
+        <div id="modal" class="modal">
+            <div class="modal-content">
+              <div class="contbox">
+                <button class="close">X</button>
+                <a href="applicationacept.html"></a>
+                <div>
+                    <form method="POST" action="">
+                        <h1> Оставить заявку</h1>
+                        <hr>
+                        <h2>Введите Имя и Фамилию:</h2>
+                        <input name="name" placeholder="ФИ" type="text">
+                        <h2>Введите адрес электронной почты:</h2>
+                        <input name="email" type="email" name="" placeholder="***@**" id="">
+                        <h2>Введите номер телефона</h2>
+                        <input name="phone" type="number" name="" id="" placeholder="7**********">
+                        <input style="margin-top: 10px;" type="submit" value="Отправить заявку" onclick="window.open('applicationacept.html')" id="">
+                    </form>
+                </div>
+              </div>
+            </div>
+          </div> 
+        
+          <?php  
+          if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone'])){
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $phone = $_POST['phone'];
+                try {
+                    $db = new PDO("mysql:host=localhost;dbname=finalproject", 'root', '');
+                    $db->exec("set names utf8");
+
+                    $data = array('name' => $name, 'email' => $email, 'phone' => $phone); 
+                    $query = $db->prepare("INSERT INTO applications (name, email, phone) values (:name, :email, :phone)");
+                    $query->execute($data);
+
+
+                } catch (PDOException $e) {
+                    print "Ошибка: " . $e->getMessage() . "<br/>";
+                }
+          }
+          ?>
+
+        <script>
+            const modal = document.querySelector('#modal');
+            const btn = document.querySelector('#openModal');
+            const close = document.querySelector('.close');
+            
+            btn.onclick = function () {
+              modal.style.display = 'block';
+            };
+            
+            close.onclick = function () {
+              modal.style.display = 'none';
+            };
+            
+            
+
+            window.onclick = function (event) {
+              if (event.target == modal) {
+                modal.style.display = 'none';
+              }
+            };
+        </script>
+
+        
+
+    </header>
+
+    <main>
+        <div style="width: 1920px; height: 50px; display: flex; justify-content: center; align-items:center; margin-top: 40px;">
+            <h1 class="rubik" style="color: white; font-size: 50px;"> ━━━━ Характеристики автомобиля ━━━━ </h1>
+        </div>
+        <div class="characteristics">
+            <div class="characteristic"><a class="rubik">700 Н/М</a></div>
+            <div class="characteristic">
+                <a class="rubik"><div class="glitch-wrapper">
+                    <div class="glitch" data-text="4.1"></div>
+                 </div><br> Разгон 0-100</a> 
+            </div>
+            <div class="characteristic"><a class="rubik">510 Л/С </a></div>
+            
+        </div>
+        <hr>
+        
+        <div class="characteristics">
+            
+            <div class="characteristic1">
+                <img src="engine.svg" alt="">
+                <a class="rubik"> Мотор <br> AMG 4.0 литра 8 цилиндров</a></div>
+            <div class="characteristic1">
+                <img style="width: 60px; height: 60px;" src="transmit.svg" alt="">
+                <a class="rubik">AMG SPEEDSHIFT MCT 9G</a> 
+            </div>
+            
+            <div class="characteristic1">
+                <img src="speedometr.svg" alt="">
+                <a class="rubik">250 КМ/Ч <br>Максимальная скорость</a>
+            </div>
+            
+        </div>
+
+
+        <section style="background-color: rgb(255, 61, 61); height: 600px;">
+            <div style="display: flex; justify-content: space-between; align-items: center;">
+                <img class="imgopis"  style="width: 780px; height: 550px; margin: 10px 0 10px 10px;" src="https://www.mercedes-benz.ca/content/dam/mb-nafta/ca/vehicles/class-c/bodystyle-cab/Class/AMG/3.%20Story%203/III.%20EXT%20Design%20and%20Packages%20TOGGLE/MBCAN-2018-C-AMG-CABRIOLET-CAROUSEL-TOP-3-3-01-DR.jpg" alt="">
+
+                    <div style="display: flex; justify-content: center; align-items: center; text-align: center; flex-direction: column;">
+                        <h1 style="color: white; width: 700px; margin-right: 140px;" class="rubik">Производительность на высоком уровне</h1> 
+                        <hr style="width: 500px; height: 5px; background-color: white; margin-right: 250px;">
+                        <p style="color: white; width: 700px; margin-right: 140px; font-weight: 800; font-size: 20px;" class="rubik">Mercedes-AMG C 63 и Mercedes-AMG C 63 S демонстрируют производительность на самом высоком уровне, который только можно увидеть и испытать. Высокотехнологичные компоненты, некоторые из которых взяты из автоспорта, и прогрессивная настройка передают его мощность на дорогу.
+                            Их чрезвычайную динамику можно узнать в каждой детали: с фейслифтингом доминирующий дизайн стал еще более четким и теперь выглядит еще более ярким и выразительным, чем когда-либо. И в интерьере ощущение AMG ощущается в каждой детали – в результате получается идеальное сочетание первого и
+                            классные материалы и спортивная атмосфера.</p> 
+                    </div>
+            </div>
+            
+
+        </section>
+        
+
+        <section  style="background-color: rgb(255, 61, 61); height: 600px; margin-top: 60px;" >
+            <div style="display: flex; justify-content: space-between; align-items: center;">
+                <div  style="display: flex; justify-content: center; align-items: center; text-align: center; flex-direction: column;">
+                    <h1 style="color: white; width: 600px; margin-right: 140px;" class="rubik">Пакет AMG Night</h1> 
+                    <hr style="width: 500px; height: 5px; background-color: white; margin-right: 250px;">
+                    <p style="color: white; width: 700px; margin-right: 140px; font-weight: 800; font-size: 20px;" class="rubik">В пакете AMG Night некоторые элементы экстерьера окрашены в глянцевый черный цвет. Таким образом, в зависимости от выбранного цвета краски, это приводит либо к сильным контрастам, либо к плавным переходам. Будь то захватывающие моменты или ощущение превосходной сдержанности: в обоих случаях вы демонстрируете свое чувство индивидуального, динамичного дизайна.</p> 
+                </div>
+                <div style="width: 850px; height: 520px; ">
+                    <img class="imgopis" style="height: 550px; width: 770px;margin-top: 10px; margin-bottom: 10px;" src="http://wp.widewallpapers.net/2k/cars/mercedes-benz/c-cabriolet/1600x900/Mercedes-AMG-C-43-4MATIC-Cabriolet-Night-Edition-2017-1600x900-002.jpg" alt="">
+                </div>
+                
+            </div>
+        </section>
+
+
+
+        
+
+
+
+    </main>
+
+    <footer>
+        <img id="logo" src="https://www.mercedes-amg.com/etc/clientlibs/amg/amg.base/assets/images/svg/amg-logo.svg" alt="">
+        <div class="footr">
+            <h1>Контакты</h1>
+                <div id="dph">
+                    <img id="phone" src="https://cdn-icons-png.flaticon.com/512/3616/3616215.png" alt=""> <p class="popin">+7(915)965-45-92</p>
+                </div>
+                <div id="dmail">
+                    <img style="border-radius: 2px;" id="mail" src="https://media.baamboozle.com/uploads/images/69723/1626327255_36566_url.png" alt=""> <p class="popin">Amg@mercedes.com</p>
+                </div>
+            <h1>Навигация</h1>
+                <a href="cars.html"  class="popin">Машины</a>
+                <a href="history.html" style="border-left: gray 1px solid; margin-left: 5px; padding-left: 3px;" class="popin">История</a>
+                <a href="map.html" style="border-left: gray 1px solid; margin-left: 5px; padding-left: 3px;" class="popin">Дилеры</a>
+            <div id="fmap">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20974.16412445365!2d9.326105435949138!3d48.91976431827194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4799cc08d338eaf3%3A0x341b17a414f2f059!2sMercedes-AMG%20GmbH!5e0!3m2!1sru!2sru!4v1653239272142!5m2!1sru!2sru" width="600" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            
+                
+        </div>
+
+        <div class="footl">
+            <h1>Мы в Соц. Сетях</h1>
+            <div id="socs">
+                <div class="inst"> <a href="https://www.instagram.com/mercedesamg/"><img id="inst" src="https://cdn-icons-png.flaticon.com/128/2111/2111463.png" alt=""></a> </div>
+                <div class="facebook"> <a href="https://www.facebook.com/MercedesAMG"><img id="facebook" src="https://cdn-icons-png.flaticon.com/128/2111/2111398.png" alt=""></a> </div>
+                <div class="twitter"> <a href="https://twitter.com/MercedesAMG"><img id="twitter" src="https://cdn-icons-png.flaticon.com/128/733/733579.png" alt=""></a> </div>
+                <div class="yt"> <a href="http://www.youtube.com/user/OfficialMercedesAMG"><img id="yt" src="https://cdn-icons-png.flaticon.com/128/733/733590.png" alt=""></a> </div>
+            </div>
+            <h1>Дополнительные ссылки</h1>
+            <div id="dopsilk">
+                <a href="https://www.mercedes-amg.com/content/global/en/footer/cookie-layer.html" class="popin">Файлы куки</a >
+                <a href="https://www.mercedes-amg.com/content/global/en/footer/data-protection.html" class="popin" style="border-left: gray 1px solid; margin-left: 5px; padding-left: 3px;">Конфиденциальность</a >
+                <a href="https://www.mercedes-amg.com/content/global/en/footer/legal-information.html" class="popin" style="border-left: gray 1px solid; margin-left: 5px; padding-left: 3px;">Юридическая информация</a >
+            </div>
+        </div>
+        
+    </footer>
+</body>
+</html>
